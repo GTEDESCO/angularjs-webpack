8c75e4c2fce4315be15de8546fafbc2fbb376de6
Gustavo Tedesco <gustavo_tedesco@hotmail.com>
2020-03-20 14:11:30 -0300

    code diretiva

--- src/app/components/code.directive.js
+++ src/app/components/code.directive.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  angular.module('myApp').directive('code', function () {
+  angular.module('myApp').directive('codediretiva', function () {
     return {   
       template: require('./code.html')
     }
